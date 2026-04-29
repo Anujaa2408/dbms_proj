83b0139ab1f37c2f41ba391c074f296d5c186ced
Izpiz06 <mdizaan1192@gmail.com>
2026-04-29 19:53:51 +0530

    added password

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ load_dotenv()
 DB_CONFIG = {
     'host':     os.getenv('DB_HOST', 'localhost'),
     'user':     os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', ''),
+    'password': os.getenv('DB_PASSWORD', 'anu_jaa@242'),
     'database': os.getenv('DB_NAME', 'HostelDB'),
 }
 
